faf16b55f91270087873d35999fcd48f0b94946b
Klinton Ballecer <klinton.ballecer@gmail.com>
2020-05-17 13:50:36 +0800

    Updates

---
+++ precache-manifest.5d00c419cb59e57d5ab3b7cd73f376f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903d5857e871d480055ec516c9b728a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54aa1fc96ce67ce03b27",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54aa1fc96ce67ce03b27",
+    "url": "/static/js/main.e6213dfb.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.952ec824ec3ce5a24c84a5d7ddd4eaa9.js"
+  "/precache-manifest.5d00c419cb59e57d5ab3b7cd73f376f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
